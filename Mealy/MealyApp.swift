e6778e448c9ea16a08b071e4a4057bc3e5785772
Bkhufa <bryankhufar@gmail.com>
2023-11-09 20:02:25 +0700

    project initialization

---
+++ Mealy/MealyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealyApp.swift
+//  Mealy
+//
+//  Created by Bryan Khufa on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MealyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
