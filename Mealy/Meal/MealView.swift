64020b1ee9e149ea4185226d767497866a320d84
Bkhufa <bryankhufar@gmail.com>
2023-11-10 23:21:50 +0700

    feat(auth) add alert for success flow

---
+++ Mealy/Meal/MealView.swift
@@ -0,0 +1,20 @@
+//
+//  MealView.swift
+//  Mealy
+//
+//  Created by Bryan Khufa on 10/11/23.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealView()
+    }
+}
